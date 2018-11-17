da02c3f84ad2ee1156888edac6ba3d26568bfcca
ldimitrov <ldimitrov070@gmail.com>
2018-11-17 01:15:55 +0100

    added models, simple data (fake) loader and service and repository

---
+++ src/main/kotlin/com/ldimitrov/beerflux/beerreactiveservice/BeerController.kt
@@ -0,0 +1,5 @@
+package com.ldimitrov.beerflux.beerreactiveservice
+
+/**
+ * @author <a href="mailto:lyuben.dimitrov@comsysto.com">dimitrov</a>
+ */
---
+++ src/main/kotlin/com/ldimitrov/beerflux/beerreactiveservice/BeerOrder.kt
@@ -0,0 +1,5 @@
+package com.ldimitrov.beerflux.beerreactiveservice
+
+import java.time.Instant
+
+class BeerOrder(val beerId: String, val whenOrdered: Instant)
