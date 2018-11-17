593e7102ab67512fc688a6d5626834b6b3e3b3d6
ldimitrov <ldimitrov070@gmail.com>
2018-11-17 01:28:53 +0100

    added controller to expose the endpoints

--- src/main/kotlin/com/ldimitrov/beerflux/beerreactiveservice/BeerController.kt
+++ src/main/kotlin/com/ldimitrov/beerflux/beerreactiveservice/BeerController.kt
@@ -1,5 +1,20 @@
 package com.ldimitrov.beerflux.beerreactiveservice
 
-/**
- * @author <a href="mailto:lyuben.dimitrov@comsysto.com">dimitrov</a>
- */
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/beers")
+class BeerController(private val service: BeerService) {
+    @GetMapping
+    fun allBeers() = service.getAllBeers()
+
+    @GetMapping("/{id}")
+    fun beerById(@PathVariable id: String) = service.findBeerById(id)
+
+    @GetMapping("/{id}/orders", produces = arrayOf(MediaType.TEXT_EVENT_STREAM_VALUE))
+    fun findOrders(@PathVariable id: String) = service.getOrdersForBeerById(id)
+}
